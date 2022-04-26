8af573e7df5da516535d9b7e1e80299f7ae8c72a
dastanmurataliev <dastan_murataliev@inbox.ru>
2022-04-26 13:55:19 +0600

    sas

--- DZ6_Dastan-13-1/dz1.es6.js
+++ DZ6_Dastan-13-1/dz1.es6.js
@@ -1,33 +1,33 @@
-// const first_name = prompt("Введите своё имя!")
-// alert(`Hello ${first_name}`);
-//
-// const arr = [23, 323, 323, 2323423]
-// const arr2 = [12423, 32421413,  395050]
-//     if (arr.length > arr2.length){
-//         alert('arr больше arr2')
-//     } else if (arr2.length > arr.length){
-//         alert('arr меньше arr2')
-//     } else {
-//         alert('они равны')
-//     }
-//
-// const colour = prompt('Выберите цвет: red, yellow, green!');
-// switch (colour) {
-//   case 'red':
-//       alert('Стой!!!')
-//       break;
-//   case 'yellow':
-//     alert('Погоди чутка')
-//     break;
-//   case 'green':
-//       alert('ыды отсюда')
-//     break;
-//   default:
-//       alert('undefined')
-//     break;
-// }
-//
-//
+const first_name = prompt("Введите своё имя!")
+alert(`Hello ${first_name}`);
+
+const arr = [23, 323, 323, 2323423]
+const arr2 = [12423, 32421413,  395050]
+    if (`arr.length>arr2.length`){
+        alert('arr больше arr2')
+    } else if (arr2.length > arr.length){
+        alert('arr меньше arr2')
+    } else {
+        alert('они равны')
+    }
+
+const colour = prompt('Выберите цвет: red, yellow, green!');
+switch (colour) {
+  case 'red':
+      alert('Стой!!!')
+      break;
+  case 'yellow':
+    alert('Погоди чутка')
+    break;
+  case 'green':
+      alert('ыды отсюда')
+    break;
+  default:
+      alert('undefined')
+    break;
+}
+
+
 
 
 
--- DZ6_Dastan-13-1/dz2.es6.js
+++ DZ6_Dastan-13-1/dz2.es6.js
@@ -1,18 +1,18 @@
-// let  star= "*"
-//
-// for (star.length<=7) {
-//     console.log(star);
-//     star+="*";
-// }
-//
-// for (let i = 1; i <=100; i++){
-//     if (i % 15 === 0) {
-//     console.log(i, "FizzBuzz");
-// } else if (i % 3 === 0) {
-//     console.log(i, "Fizz");
-// } else if (i % 5 === 0) {
-//     console.log(i, "Buzz");
-// } else {
-//     console.log(i)
-// }
-// }
+let  star= "*"
+
+while (star.length<=7) {
+    console.log(star);
+    star+="*";
+}
+
+for (let i = 1; i <=100; i++){
+    if (i % 15 === 0) {
+    console.log(i, "FizzBuzz");
+} else if (i % 3 === 0) {
+    console.log(i, "Fizz");
+} else if (i % 5 === 0) {
+    console.log(i, "Buzz");
+} else {
+    console.log(i)
+}
+}
